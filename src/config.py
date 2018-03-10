5e357642d28bcff7406a48d6bb24b22b7190c5af
Sushil Thapa <xsushil@outlook.com>
2018-03-11 00:33:16 +0545

    Add default hyperparameters and constants

---
+++ src/config.py
@@ -0,0 +1,18 @@
+'''
+Default Parameters
+'''
+FILTERS = [
+    [4, 1, 2],
+    [2, 2, 4],
+    [1, 4, 4],
+    [1, 4, 1]
+]
+
+NO_CUDA = True
+MSG_LEN = 16
+KEY_LEN = 16
+BATCH_SIZE = 512
+NUM_EPOCHS = 60
+LEARNING_RATE = 0.0008
+RANDOM_SEED = 42
+LOG_INTERVAL = 10
