430add5fdef349297a11c41e1192a31c8b6ac6c8
Sushil Thapa <xsushil@outlook.com>
2018-04-16 22:29:26 +0545

    Add Batch Mode for data generation

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,13 +1,13 @@
 import numpy as np
 import torch
-from data.config import MSG_LEN, KEY_LEN, CHAR_SET
+from data.config import MSG_LEN, KEY_LEN, CHAR_SET, BATCH_SIZE
 
-def generate_sequence(n):
-    seq = np.random.choice(CHAR_SET, n) #generate random n-bit long sequences
-    seq = torch.from_numpy(seq.reshape(n,1))
+def generate_sequence(n_batch, n_char):
+    seq = np.random.choice(CHAR_SET, n_char*n_batch) #generate random n-bit long sequences
+    seq = torch.from_numpy(seq.reshape(n_batch,1,n_char))
     return seq
 
 def get_data():
-    plain = generate_sequence(MSG_LEN)
-    key = generate_sequence(KEY_LEN)
+    plain = generate_sequence(BATCH_SIZE, MSG_LEN)
+    key = generate_sequence(BATCH_SIZE, KEY_LEN)
     return (plain, key)
