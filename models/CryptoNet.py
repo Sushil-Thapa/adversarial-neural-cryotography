323f2bb9485eb4019723c3cdb45adedd0d501562
Sushil Thapa <xsushil@outlook.com>
2018-05-11 17:10:23 +0545

    Add Linear Layers

--- models/CryptoNet.py
+++ models/CryptoNet.py
@@ -15,26 +15,28 @@ class ABCryptoNet(nn.Module):
         super(ABCryptoNet,self).__init__()
         self.fc_in =MSG_LEN + KEY_LEN
         self.fc_out = self.fc_in
-        self.linear = nn.Linear(self.fc_in,self.fc_out)
+        self.l1 = nn.Linear(self.fc_in,self.fc_out)
+        self.l2 = nn.Linear(self.fc_out,int(self.fc_out / 2))
         self.sigmoid = nn.Sigmoid()
+        self.tanh = nn.Tanh()
 
-        self.net = nn.Sequential(
-            nn.Linear(self.fc_in,self.fc_out),
-            nn.Sigmoid(),
-            nn.Conv1d(1,2,4,stride=1),
-            nn.Sigmoid(),
-            nn.Conv1d(2,4,2,stride=2),
-            nn.Sigmoid(),
-            nn.Conv1d(4,4,1,stride=1),
-            nn.Sigmoid(),
-            nn.Conv1d(4,1,1,stride=1),
-            nn.Tanh()
-        )
+        # self.net = nn.Sequential(
+        #     nn.Conv1d(self.fc_out*1,self.fc_out*2,4,stride=1),
+        #     nn.Sigmoid(),
+        #     nn.Conv1d(self.fc_out*2,self.fc_out*4,2,stride=2),
+        #     nn.Sigmoid(),
+        #     nn.Conv1d(self.fc_out*4,self.fc_out*4,1,stride=1),
+        #     nn.Sigmoid(),
+        #     nn.Conv1d(self.fc_out*4,self.fc_out*1,1,stride=1),
+        #     nn.Tanh()
+        # )
     def forward(self,input):
         # input_fc = input.view(-1, self.fc_in) #feed to FC
-        # import pdb; pdb.set_trace()
-        _out = self.linear(self.linear(input.squeeze(1)))
-        output = self.net(_out.unsqueeze(1))
+        # import pdb; pd b.set_trace()
+        _out = self.sigmoid(self.l1(input.squeeze(1)))
+        _out = self.tanh(self.l2(_out))
+        # output = self.net(_out.unsqueeze(1))
+        output = _out.unsqueeze(1)
         print('one pass AB', output.size())
         return output
 
@@ -48,21 +50,27 @@ class ECryptoNet(nn.Module):
         super(ECryptoNet,self).__init__()
         self.fc_in =MSG_LEN
         self.fc_out = 2 * MSG_LEN
-        self.linear = nn.Linear(self.fc_in,self.fc_out)
+        # self.linear = nn.Linear(self.fc_in,self.fc_out)
+        self.l1 = nn.Linear(self.fc_in,self.fc_out)
+        self.l2 = nn.Linear(self.fc_out,int(self.fc_out / 2))
         self.sigmoid = nn.Sigmoid()
-        self.net = nn.Sequential(
-            nn.Conv1d(1,2,4,stride=1),
-            nn.Sigmoid(),
-            nn.Conv1d(2,4,2,stride=2),
-            nn.Sigmoid(),
-            nn.Conv1d(4,4,1,stride=1),
-            nn.Sigmoid(),
-            nn.Conv1d(4,1,1,stride=1),
-            nn.Tanh()
-        ) #eve stride [1,1,1,1] in github.com/rfratila/Adversarial-Neural-Cryptography/blob/master/net.py
+        self.tanh = nn.Tanh()
+        # self.net = nn.Sequential(
+        #     nn.Conv1d(1,2,4,stride=1),
+        #     nn.Sigmoid(),
+        #     nn.Conv1d(2,4,2,stride=2),
+        #     nn.Sigmoid(),
+        #     nn.Conv1d(4,4,1,stride=1),
+        #     nn.Sigmoid(),
+        #     nn.Conv1d(4,1,1,stride=1),
+        #     nn.Tanh()
+        # ) #eve stride [1,1,1,1] in github.com/rfratila/Adversarial-Neural-Cryptography/blob/master/net.py
 
     def forward(self,input):
-        _out = self.linear(self.linear(input.squeeze(1)))
-        output = self.net(_out.unsqueeze(1))
+        # _out = self.linear(self.linear(input.squeeze(1)))
+        # output = self.net(_out.unsqueeze(1))
+        _out = self.sigmoid(self.l1(input.squeeze(1)))
+        _out = self.tanh(self.l2(_out))
+        output = _out.unsqueeze(1)
         print('one pass E',output.size())
         return output
